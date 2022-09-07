987c1bd47d057953d6b2b6fd644a26babb7cf93f
ProPratik1405 <sariapratik@gmail.com>
2022-09-07 17:57:23 +0530

    The SDK is working

--- packages/test/pages/_app.tsx
+++ packages/test/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import {} from "@wordcel/wordcel-ts";
+import { Connection } from "@wordcel/wordcel-ts";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
