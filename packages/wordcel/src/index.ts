dd96a27a3621b02a46898fdb38c66114ef6a83e0
vjgpt <vjgupta57@gmail.com>
2022-10-07 08:33:08 +0000

    Deploying to gh-pages from @ Wordcel/wordcel-sdk@325b05f38d88735aae86d19033f0d3c35e88a812 🚀

--- packages/wordcel/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./gm";
-export { SDK } from "./sdk";
-export { Profile } from "./profile";
-export { Post } from "./post";
-export { Connection } from "./connection";
--- packages/wordcel/tests/gm.spec.ts
+++
@@ -1,9 +0,0 @@
-import { gm } from "../src";
-import { expect } from "chai";
-
-describe("gm", async () => {
-  it("should say gm", async () => {
-    const value = gm();
-    expect(value).to.equal("gm");
-  });
-});
